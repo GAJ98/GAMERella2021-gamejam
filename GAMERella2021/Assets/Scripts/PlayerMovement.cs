cb909e8c576557e20da12dfaf04db0c4bbbdc2b9
GAJ98 <gregoryalexisjohn@live.in>
2021-07-10 03:54:15 -0400

    Added jump with limiter in code

--- GAMERella2021/Assets/Scripts/PlayerMovement.cs
+++ GAMERella2021/Assets/Scripts/PlayerMovement.cs
@@ -5,21 +5,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] public float speed = 0.1f;
+    [SerializeField] public float walkSpeed = 0.9f;
+    [SerializeField] public float crouchSpeed = 1.3f;
+    [SerializeField] public float runSpeed = 1.3f;
+    [SerializeField] public float jumpForce = 10f;
     [SerializeField] private Animator anim;
+    [SerializeField] private LayerMask groundLayer;
+    private float speed;
     private Vector3 prevMove = Vector3.zero;
+    private Rigidbody rb;
+    private Collider capsuleCollider;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<Collider>();
+        speed = walkSpeed;
+    }
 
     private void Update()
     {
         Move();
-
         Crouch();
+        Jump();
     }
 
     private void Move()
     {
-        
-
         if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.D))
         {
             float xDirection = Input.GetAxis("Horizontal");
@@ -45,10 +57,26 @@ public class PlayerMovement : MonoBehaviour
         if(Input.GetKey(KeyCode.LeftControl))
         {
             anim.SetBool("Crouched", true);
+            speed = crouchSpeed;
         }
         else
         {
             anim.SetBool("Crouched", false);
+            speed = walkSpeed;
         }
+    }
+
+    private void Jump()
+    {
+        if (IsGrounded() && Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.CapsuleCast(capsuleCollider.bounds.center, capsuleCollider.bounds.size, 0f,
+            Vector3.down, 1f, groundLayer);
     }
 }
